3d5e22fef21fa53d0a404851681ab016ba22895d
wangj <wangj@DESKTOP-8A998EI>
2017-02-01 19:58:33 +0800

    1.1

--- 文件批处理/Form1.Designer.cs
+++ 文件批处理/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
@@ -42,26 +46,26 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(97, 112);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(73, 37);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(579, 25);
+            this.textBox1.Size = new System.Drawing.Size(442, 28);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(97, 218);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Location = new System.Drawing.Point(73, 119);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(579, 25);
+            this.textBox2.Size = new System.Drawing.Size(442, 28);
             this.textBox2.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(685, 112);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(523, 35);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.Size = new System.Drawing.Size(48, 30);
             this.button1.TabIndex = 2;
             this.button1.Text = "打开";
             this.button1.UseVisualStyleBackColor = true;
@@ -69,10 +73,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(685, 218);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(523, 119);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 29);
+            this.button2.Size = new System.Drawing.Size(48, 30);
             this.button2.TabIndex = 3;
             this.button2.Text = "打开";
             this.button2.UseVisualStyleBackColor = true;
@@ -81,42 +85,42 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(24, 116);
+            this.label1.Font = new System.Drawing.Font("思源黑体 Regular", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(13, 40);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.Size = new System.Drawing.Size(52, 21);
             this.label1.TabIndex = 5;
             this.label1.Text = "文件夹";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(24, 221);
+            this.label2.Font = new System.Drawing.Font("思源黑体 Regular", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(13, 122);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.Size = new System.Drawing.Size(52, 21);
             this.label2.TabIndex = 6;
             this.label2.Text = "文件名";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(452, 321);
+            this.label3.Font = new System.Drawing.Font("思源黑体 Regular", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(388, 292);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.Size = new System.Drawing.Size(66, 21);
             this.label3.TabIndex = 7;
             this.label3.Text = "填充数据";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(685, 322);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(523, 206);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 29);
+            this.button3.Size = new System.Drawing.Size(48, 30);
             this.button3.TabIndex = 8;
             this.button3.Text = "开始";
             this.button3.UseVisualStyleBackColor = true;
@@ -124,10 +128,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(685, 409);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Location = new System.Drawing.Point(523, 287);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(100, 29);
+            this.button4.Size = new System.Drawing.Size(48, 30);
             this.button4.TabIndex = 9;
             this.button4.Text = "重置";
             this.button4.UseVisualStyleBackColor = true;
@@ -135,9 +139,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(832, 551);
+            this.ClientSize = new System.Drawing.Size(584, 331);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label3);
@@ -147,8 +152,9 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("思源黑体 Regular", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- 文件批处理/Form1.cs
+++ 文件批处理/Form1.cs
@@ -14,7 +14,8 @@ namespace 文件批处理
 {
     public partial class Form1 : Form
     {
-        string foldPath;
+        //string foldPath;
+        string[] foldPaths;
         string[] filesPath;
 
         public Form1()
@@ -22,15 +23,52 @@ namespace 文件批处理
             InitializeComponent();
         }
 
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            List<string> dirs = new List<string>();
+            List<string> files = new List<string>();
+            String[] fileNames = (String[])e.Data.GetData(DataFormats.FileDrop);
+            for (int i = 0; i < fileNames.Length; i++)
+            {
+                if (Directory.Exists(fileNames[i]))
+                {
+                    textBox1.Text += fileNames[i] + "; ";
+                    dirs.Add(fileNames[i]);
+                }
+                else if (File.Exists(fileNames[i]))
+                {
+                    textBox2.Text += fileNames[i] + "; ";
+                    files.Add(fileNames[i]);
+                }
+            }
+            foldPaths = dirs.ToArray();
+            filesPath = files.ToArray();
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog folder = new FolderBrowserDialog();
             folder.Description = "请选择文件路径";
+            foldPaths = new string[1];
             if (folder.ShowDialog() == DialogResult.OK)
             {
-                foldPath = folder.SelectedPath;
+                foldPaths[0] = folder.SelectedPath;
                 //MessageBox.Show("已选择文件夹:" + foldPath, "选择文件夹提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                textBox1.Text = foldPath;
+                textBox1.Text = foldPaths[0];
                 textBox2.Enabled = false;
                 button2.Enabled = false;
             }
@@ -61,13 +99,31 @@ namespace 文件批处理
                         {
                             data = (byte)Convert.ToInt32(textBox3.Text, 16);
                         }*/
-            if (textBox1.Text != "")
+            if (textBox1.Text != "" && textBox2.Text != "")
             {
-                int a = dircode.dirCl(foldPath);
+                int a = 1;
+                foreach (string foldPath in foldPaths)
+                    a = dircode.dirCl(foldPath);
+                int length = filesPath.Length;
+                FileInfo[] filesP = new FileInfo[length];
+                for (int i = 0; i < length; i++)
+                {
+                    filesP[i] = new FileInfo(filesPath[i]);
+                }
+                dircode.FilePcl(filesP);
+                textBox1.Text = textBox2.Text = "";
+                MessageBox.Show("完成", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (textBox1.Text != "")
+            {
+                int a = 1;
+                foreach (string foldPath in foldPaths)
+                    a = dircode.dirCl(foldPath);
                 if (a == 0) 
                 { 
                     MessageBox.Show("完成", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information); 
                 }
+                textBox1.Text = textBox2.Text = "";
             }
             else if (textBox2.Text != "") 
             {
